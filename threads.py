64f91babb717edad334cd20584e48a5dd9d54577
benhacks300 <benjamin.ruiz.salvatierra@gmail.com>
2023-04-06 21:08:47 -0400

    Threads

--- threads.py
+++ threads.py
@@ -11,8 +11,20 @@ def contador_independiente(valor: int):
     for i in range(1, valor + 1):
         print(f"{nombre_thread}: {i}...\n")
         time.sleep(1) # La función sleep nos permite realizar una simulación del tiempo te trabajo del thread
-    print("He terminado de contar :3")
+    print("He terminado de contar :3\n")
 
 # Para crear un thread, este se debe instanciar como una clase común y corriente
-mi_hilo = threading.Thread(target=contador_independiente, kwargs={"valor": 20}, name="Benjamin")
+mi_hilo = threading.Thread(target=contador_independiente, kwargs={"valor": 10}, name="Benjamin")
 mi_hilo.start()
+mi_hilo.join()
+
+tiempo = 5
+hilo_con_tiempo_de_espera = threading.Timer(tiempo, contador_independiente, args=(10,)) # Este hilo comienza su ejecución luego de 5 segundos
+hilo_con_tiempo_de_espera.start() 
+for i in range(1, tiempo+1):
+    print(f"Contando... {i}")
+    time.sleep(1)
+print()
+hilo_con_tiempo_de_espera.join()
+
+print("MainThread ha terminado su pega")
